8131e1d745d0a5efc8097dc37678d29d59b08dcd
kodjoxmiezan-dotcom <kodjoxmiezan@gmail.com>
2026-09-17 07:38:06 +0000

    Cible ATHENEE v2.0.1 sur Moodle 5.2.3

--- local/athenee/version.php
+++ local/athenee/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_athenee';
-$plugin->version = 2026091700;
-$plugin->requires = 2024042200; // Moodle 4.4+
+$plugin->version = 2026091701;
+$plugin->requires = 2026042003; // Moodle 5.2.3+
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.0';
+$plugin->release = '2.0.1';
