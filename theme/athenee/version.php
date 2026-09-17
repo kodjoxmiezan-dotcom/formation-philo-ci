833d5d51c607523d5ed238356c5ae0560409b09c
kodjoxmiezan-dotcom <kodjoxmiezan@gmail.com>
2026-09-17 06:48:35 +0000

    Ajoute la métadonnée du thème ATHENEE

---
+++ theme/athenee/version.php
@@ -0,0 +1,7 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$plugin->component='theme_athenee';
+$plugin->version=2026091700;
+$plugin->requires=2024042200;
+$plugin->maturity=MATURITY_STABLE;
+$plugin->release='2.0.0';
