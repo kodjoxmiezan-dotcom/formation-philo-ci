98c537fd900a6afb5775584a3ca0830658a3ec2f
kodjoxmiezan-dotcom <kodjoxmiezan@gmail.com>
2026-09-17 07:38:16 +0000

    Cible le thème ATHENEE v2.0.1 sur Moodle 5.2.3

--- theme/athenee/version.php
+++ theme/athenee/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->component='theme_athenee';
-$plugin->version=2026091700;
-$plugin->requires=2024042200;
+$plugin->version=2026091701;
+$plugin->requires=2026042003;
 $plugin->maturity=MATURITY_STABLE;
-$plugin->release='2.0.0';
+$plugin->release='2.0.1';
