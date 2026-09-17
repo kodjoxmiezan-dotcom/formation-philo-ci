fe22210f12d84853877eb3e3f5871e8ea55d94c1
kodjoxmiezan-dotcom <kodjoxmiezan@gmail.com>
2026-09-17 06:48:27 +0000

    Ajoute le modèle de configuration privée

---
+++ local/athenee/secrets.example.php
@@ -0,0 +1,8 @@
+<?php
+// Copy to secrets.php on the Moodle server and fill server-side values.
+return [
+    'edgeurl' => 'https://YOUR_PROJECT.supabase.co/functions/v1/athenee-license',
+    'edgekey' => 'CHANGE_ME',
+    'sheetssecret' => 'CHANGE_ME',
+    'sheetswebhook' => 'https://script.google.com/macros/s/.../exec',
+];
