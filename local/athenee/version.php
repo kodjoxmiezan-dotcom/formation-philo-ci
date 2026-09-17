d2401b0bc2dd4fa595d2350192f96d7e8dc293a5
kodjoxmiezan-dotcom <kodjoxmiezan@gmail.com>
2026-09-17 06:48:18 +0000

    Ajoute la métadonnée du plugin local ATHENEE

---
+++ local/athenee/version.php
@@ -0,0 +1,7 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$plugin->component = 'local_athenee';
+$plugin->version = 2026091700;
+$plugin->requires = 2024042200; // Moodle 4.4+
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.0.0';
